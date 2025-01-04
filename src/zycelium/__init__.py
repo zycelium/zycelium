6feacbf4aced5b6f984576752cebc42abea5b3ed
Harshad Sharma <harshad@sharma.io>
2025-01-05 01:16:24 +0530

    Clean up example docs.

--- src/zycelium/__init__.py
+++ src/zycelium/__init__.py
@@ -1,34 +1,3 @@
-"""Zycelium: Personal Communication and Automation Network
-
-This module provides tools for building personal automation networks.
-
-Examples
---------
-Basic usage example:
-
->>> def add(a: int, b: int) -> int:
-...     '''Add two numbers together
-...
-...     Examples
-...     --------
-...     >>> add(2, 3)
-...     5
-...     >>> add(-1, 1)
-...     0
-...     '''
-...     return a + b
-
->>> def multiply(a: int, b: int) -> int:
-...     '''Multiply two numbers
-...
-...     Examples
-...     --------
-...     >>> multiply(2, 3)
-...     6
-...     >>> multiply(-2, 3)
-...     -6
-...     '''
-...     return a * b
-"""
+"""Zycelium: Personal Communication and Automation Network"""
 
 __version__ = "2025.0.0"
